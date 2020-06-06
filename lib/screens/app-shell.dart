76360844e7f9208cdfe50072f30bf6d4498b900f
github_ajacin <arunjacob2@gmail.com>
2020-06-06 11:11:15 +0530

    Folder strtucture changes and default widget name changes

---
+++ lib/screens/app-shell.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+
+class AppShell extends StatefulWidget {
+  AppShell({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _AppShellState createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many timess:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
